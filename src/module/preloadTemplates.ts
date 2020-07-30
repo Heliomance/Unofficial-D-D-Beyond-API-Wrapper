fe7327d8a0f0b1ca18d59c7f7572a2503ec213dc
Tamsin Coton <tamcoton@hotmail.co.uk>
2020-07-30 19:03:50 +0100

    Initialise

---
+++ src/module/preloadTemplates.ts
@@ -0,0 +1,7 @@
+export const preloadTemplates = async function() {
+	const templatePaths = [
+		// Add paths to "modules/unofficial-ddb-api-wrapper/templates"
+	];
+
+	return loadTemplates(templatePaths);
+}
